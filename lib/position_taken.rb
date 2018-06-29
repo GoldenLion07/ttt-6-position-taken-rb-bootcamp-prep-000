ecd2a2d057d3ffc31c869a1808ccb6a71179d61b
gmat191@gmail.com <gmat191@gmail.com>
2018-06-29 22:46:21 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,6 @@
 # code your #position_taken? method here!
-board = []
+board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
+
 def position_taken(board, index)
   ! (board[index] ==  " " || board[index] == "" || board[index] == nil) 
 end
